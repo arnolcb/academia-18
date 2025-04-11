084b77fabf4bae937df26bbde386240789a56e05
unknown <arnolcb14@gmail.com>
2025-04-11 07:53:50 -0500

    remove render

--- academia-18/vite.config.js
+++ academia-18/vite.config.js
@@ -1,24 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import prerender from 'vite-plugin-prerender' // 👈 nuevo import
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    prerender({
-      staticDir: 'dist',
-      routes: [
-        '/',
-        '/about',
-        '/matriculate',
-        '/rendir-test',
-        '/aula-virtual',
-        '/cursos'
-      ]
-    })
   ],
   resolve: {
     alias: {
