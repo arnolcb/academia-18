96b1257665d12d7a2ced27c88e340904c8f151ca
unknown <arnolcb14@gmail.com>
2025-10-06 18:14:36 -0500

    delete: icon daily challenge

--- academia-18/src/views/DashboardView.vue
+++ academia-18/src/views/DashboardView.vue
@@ -317,22 +317,7 @@
             <div class="materiales-sidebar materiales-vip">
               <div class="reto-diario-card">
                 <button @click="navegarARetoDiario" class="reto-diario-btn">
-                  <div class="reto-icon">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <circle cx="12" cy="12" r="10"></circle>
-                      <polyline points="12 6 12 12 16 14"></polyline>
-                    </svg>
-                  </div>
+                  
                   <div class="reto-content">
                     <h3>Reto Diario RV</h3>
                     <p>Practica y mejora en Razonamiento Verbal</p>
