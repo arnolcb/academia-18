3fad3cb8bbf57836a171ff3fc86b008c9ec136cf
unknown <arnolcb14@gmail.com>
2025-10-09 09:39:09 -0500

    resultados sim #5 vip

--- academia-18/src/views/DashboardView.vue
+++ academia-18/src/views/DashboardView.vue
@@ -214,7 +214,7 @@
             <!-- Grid combinado de cursos VIP y simulacro -->
             <div class="cursos-grid">
               <!-- VERSIÓN 1: RENDIR SIMULACRO (ACTIVA) -->
-
+<!--
               <div
                 class="curso-card curso-vip simulacro-vip-especial"
                 @click="navegarASimulacroVip"
@@ -243,9 +243,9 @@
                   </p>
                 </div>
               </div>
-
+-->
               <!-- VERSIÓN 2: VER RESULTADOS (COMENTADA)-->
-              <!--
+              
               <div
                 class="curso-card curso-vip simulacro-vip-especial simulacro-resultados"
                 @click="navegarAResultadosVip"
@@ -269,11 +269,11 @@
                   </div>
                 </div>
                 <div class="curso-info">
-                  <h2>SIMULACRO #4 RESULTADOS</h2>
+                  <h2>SIMULACRO #5 RESULTADOS</h2>
                   <p>Consulta tu ranking y desempeño en el simulacro VIP</p>
                 </div>
               </div>
--->
+
               <!-- Las tarjetas de curso VIP existentes -->
               <div
                 v-for="curso in cursosVip"
